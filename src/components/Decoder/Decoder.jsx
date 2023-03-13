3238183014166f69f9551867aae638b21124efef
Nikrbb <avnnnyr13@gmail.com>
2023-03-13 22:24:04 +0200

    refactored Decoder component, added StoryBook components

--- src/components/Decoder/Decoder.jsx
+++ src/components/Decoder/Decoder.jsx
@@ -1,179 +1,172 @@
 import './decoder.scss'
-import React from "react";
 import axios from 'axios';
 import Button from "@avtopro/button/dist/index"
 import Select, { Option } from "@avtopro/select/dist/index";
 import Modal from "@avtopro/modal/dist/index";
+import TextInput from "@avtopro/text-input/dist/index";
+import PlaceholderRobot from '@avtopro/placeholder-robot/dist/index';
+import React, { useState, useEffect } from 'react';
 
-class Decoder extends React.Component {
-    constructor(props) {
-        super(props);
+function Decoder() {
+    const [latestSearch, setLatestSearch] = useState();
+    const [vinInputValue, setInputValue] = useState(null);
+    const [recentlyViewedList, setRecentlyViewedList] = useState([]);
+    const [vinData, setVinData] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [isShownModal, toggleModal] = useState(false);
 
-        this.state = {
-            vinInputValue: null,
-            recentlyViewedList: [],
-            vinData: {},
-            loading: true,
-            isShownModal: false
-        }
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         let viewedList = JSON.parse(localStorage.getItem('viewedList'));
 
-        this.setState( { recentlyViewedList: viewedList ? viewedList : [], loading: false });
+        setRecentlyViewedList( viewedList ? viewedList : []);
+        setLoading(false);
+    }, [])
+
+    const getVinData = (vin) => {
+        // here we validate input value only if function was called
+        // by btn "send request" not by recently viewed item
+        if (!vin) if (!validateInput()) return;
+
+        const vinToDecode = vin ? vin : vinInputValue;
+        setLoading(true);
+
+        sendRequest(vinToDecode)
     };
-    validateInput() {
-        let obj = {};
-        if (!this.state.vinInputValue) {
-
-            obj.message =  "Vin code can`t be empty";
-            this.setState({vinData: obj});
-            return false;
-
-        } else if (this.state.vinInputValue.length > 17) {
-
-            obj.message =  "Vin code can`t have more than 17 characters";
-            this.setState({vinData: obj});
-            return false;
-        // eslint-disable-next-line
-        } else if ( /^(?=.*[!@#$%^&(),.+=/\/\]\[{}?><":;|])/.test(this.state.vinInputValue) ) {
-
-            obj.message =  "Vin code can`t have forbidden characters";
-            this.setState({vinData: obj});
-            return false;
-
-        } else return true;
-
-    };
-    sendRequest(vinToDecode) {
+    const sendRequest = (vinToDecode) => {
         axios.get( `https://vpic.nhtsa.dot.gov/api/vehicles/decodevin/${ vinToDecode }?format=json` )
-            .then(( response) => {
+            .then((response) => {
 
                 let vinResponse = {
                     searchedVin: response.data.SearchCriteria,
                     message: response.data.Message,
                     results: response.data.Results.filter(elem => !!elem.Value)
                 }
-                this.setState({ vinData: vinResponse });
+                setVinData(vinResponse);
+                if (!recentlyViewedList.includes( vinToDecode )) {
 
-                if (!this.state.recentlyViewedList.includes(vinToDecode)) {
-
-                    let viewedList = [...this.state.recentlyViewedList, vinToDecode];
+                    let viewedList = [...recentlyViewedList, vinToDecode];
                     if (viewedList.length > 5) viewedList.splice(0, 1);
 
-                    this.setState({ recentlyViewedList: viewedList });
+                    setRecentlyViewedList ( viewedList );
+                    
                     localStorage.setItem('viewedList', JSON.stringify(viewedList));
 
                 }
-                this.setState({loading: false});
+                setLatestSearch( vinToDecode );
+                setLoading(false);
 
             }).catch( error => console.log(error) );
     };
-    getVinData(vin) {
+    const validateInput = () => {
+        let obj = {};
+        if (!vinInputValue) {
 
-        // here we validate input value only if function was called
-        // by btn "send request" not by recently viewed item
-        if (!vin) if (!this.validateInput()) return;
+            obj.message =  "Vin code can`t be empty";
+            setVinData(obj);
+            return false;
 
-        const vinToDecode = vin ? vin : this.state.vinInputValue;
+        } else if (vinInputValue.length > 17) {
 
-        this.setState({ loading: true });
+            obj.message =  "Vin code can`t have more than 17 characters";
+            setVinData(obj);
+            return false;
+        // eslint-disable-next-line
+        } else if ( /^(?=.*[!@#$%^&(),.+=/\/\]\[{}?><":;|])/.test(vinInputValue) ) {
 
-        this.sendRequest(vinToDecode)
+            obj.message =  "Vin code can`t have forbidden characters";
+            setVinData(obj);
+            return false;
+
+        } else return true;
+
     };
-
-    render() {
-
-        const MsgFromRequest = () => {
-            if (!Object.keys(this.state.vinData).length) return (
-                <div className="decoder__empty-msg">
-                    <span>Area for message from response</span>
-                </div>
-            )
-            else return <div className="decoder__message">
-                <span>response message</span>
-                <p >{this.state.vinData.message}</p>
+    function  MsgFromRequest(props)  {
+        if (!Object.keys(props.vinData).length) return (
+            <div className="decoder__empty-msg">
+                <span>Area for message from response</span>
             </div>
-        }
-        
-        const RecentlyViewedVinCodes = () => {
-            
-            if (this.state.recentlyViewedList.length) {
-                return (
-                    <div className="decoder__viewed">
-                        <h5>Recently viewed:</h5>
+        )
+        else return <div className="decoder__message">
+            <span>response message</span>
+            <p >{props.vinData.message}</p>
+        </div>
+    }
+    function RecentlyViewedVinCodes() {
+            return (
+                <div className="decoder__viewed">
+                    <h5>Recently viewed:</h5>
 
-                        <Select onChange={(_, vin) => this.getVinData(vin[0])}>
-                            {this.state.recentlyViewedList.map(vin => 
+                    <Select 
+                        onChange={(_, vin) => getVinData(vin[0])} 
+                        value={latestSearch}
+                        placeholder={!recentlyViewedList[0] ? "empty" : "select"}
+                        blockSize="sm"
+                        >
+                            {recentlyViewedList.map(vin => 
                             <Option value={vin} key={vin}>{vin}</Option>
                             )}
-                        </Select>
+                    </Select>
+                     
+                </div>
+            )
+    };
+    function VinCodeDataList () {
+        const { searchedVin } = vinData;
 
-                        {/* <ul>
-                            {this.state.recentlyViewedList.map(vin => <li key={vin}>
-                                <span className="decoder__viewed-link" onClick={() => this.getVinData(vin) }>{ vin }</span>
-                            </li>)}
-                        </ul> */}
-                         
-                    </div>
-                )
-            } else return null
-        };
+        if (loading || !vinData.results) return null;
+        else return <ul className="decoder__data-list">
+            {vinData.results.map((elem, index) => {
+                return <li className="decoder__data-item" key={index + searchedVin}>
+                    <div className="decoder__title">{elem.Variable + ':'}</div>
+                    <div className="decoder__description">{elem.Value}</div>
+                </li>
+            })}
+        </ul>
+    };
 
-        const VinCodeDataList = () => {
-            const { searchedVin } = this.state.vinData;
-
-            if (this.state.loading || !this.state.vinData.results) return null;
-            else return <ul className="decoder__data-list">
-                {this.state.vinData.results.map((elem, index) => {
-                    return <li className="decoder__data-item" key={index + searchedVin}>
-                        <div className="decoder__title">{elem.Variable + ':'}</div>
-                        <div className="decoder__description">{elem.Value}</div>
-                    </li>
-                })}
-            </ul>
-        };
-
-        return this.state.isShownModal 
-        ?   <Modal>
-                 <div>Модалка</div>
-            </Modal> 
-        : <section className="decoder section">
+    return  <section className="decoder section">
             <div className="decoder__head">
 
                 <div className='decoder__input-wrapper'>
-                    <input
-                        className="decoder__input"
-                        placeholder="type vin to decode"
-                        onChange={ (e) => this.setState({ vinInputValue: e.target.value }) }
-                        type="text"/>
+                    <TextInput onChange={ (e) => setInputValue( e.target.value ) } blockSize="sm" placeholder="type vin to decode" />
                     <Button 
                         blockSize="sm" 
                         theme="blue"
-                        onClick={ () => this.getVinData() }>
-                    {this.state.loading ? "loading" : "Send request"}
+                        onClick={ () => getVinData() }>
+                    { loading ? "loading" : "Send request"}
                     </Button>
                 </div>
 
-                <MsgFromRequest/>
+                <MsgFromRequest vinData={vinData}/>
 
             </div>
 
             
-            <RecentlyViewedVinCodes/>
+            <RecentlyViewedVinCodes recentlyViewedList={recentlyViewedList}/>
 
             <VinCodeDataList/>
 
-            { !this.state.vinData.results || this.state.loading ?
+            { !vinData.results || loading ?
                 null :
                 <button className="decoder__clear-btn"
-                        onClick={() => this.setState( { vinData: {}} )}>
+                        onClick={() => toggleModal(true)}>
                     clear
-                </button>}
+                </button> }
+
+
+            { isShownModal
+            ?   <Modal size="wide" onClose={() => {toggleModal(!isShownModal) }}>
+                    <div>
+                        <p style={{textAlign: "center"}}>Are you sure you want to clear vehicle data?</p>
+                        <Button onClick={() => {setVinData({}); toggleModal(!isShownModal)}} style={{position: "absolute", bottom: '10px', right: '10px'}} theme="danger" blockSize="sm">delete</Button>
+                        <PlaceholderRobot />
+                    </div>
+                </Modal> 
+            : null }
+
+
 
         </section>
-    }
 }
 
-export default Decoder;
+export default Decoder
--- src/routes/VinDecoderPage.jsx
+++ src/routes/VinDecoderPage.jsx
@@ -1,6 +1,8 @@
+
 import Decoder from "../components/Decoder/Decoder";
 
 const VinDecoderPage = () => {
         return <Decoder/>
+
 }
 export default VinDecoderPage;
