383fdcecd6e075428b8dba40c5275d0eeeab6d92
Nikrbb <avnnnyr13@gmail.com>
2023-03-15 09:46:10 +0200

    fix routes

--- src/components/Navigation/Nav.jsx
+++ src/components/Navigation/Nav.jsx
@@ -33,8 +33,8 @@ const Nav = () => {
     return <div>
         <nav className='nav'>
             <Link
-                className={"nav__link" + (location.pathname === "/decoder" ? " active" : "")}
-                to='/decoder'>
+                className={"nav__link" + (location.pathname === "/" ? " active" : "")}
+                to='/'>
                 Vin Decoder
             </Link>
             <Link
